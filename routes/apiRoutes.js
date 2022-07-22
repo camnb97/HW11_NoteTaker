666558765659c5f840b7dcae3f258a198cd712f7
Cameron Buss <camnb97@gmail.com>
2022-07-21 18:27:56 -0700

    bug fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,48 @@
 const app = require('express').Router();
-
+const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
 
 //app is our istance of the express package
 // .get is 
 // res.send is the type of response we wat from the request
 
-module.exports = function(app) {
-  app.get('/api/notes', (req, res) => {
-    // should read the db.json file and return all saved notes as JSON.
-    console.log("hit");
-  fs.readFile('/db/db.json', (err, data) => {
-    if (err) throw err;
-    dbData = JSON.parse(data);
-    res.send(dbData); 
-    });
-    console.log("hit")
-  })
-  
-app.post('/api/notes', (req, res) => {
-    //this should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-    const userNotes = req.body;
-
-    fs.readFile('/db/db.json', (err, data) => {
+app.get('/notes', (req, res) => {
+  // should read the db.json file and return all saved notes as JSON.
+  console.log("hit");
+  readFileAsync('db/db.json', 'utf8',
+    (err, data) => {
       if (err) throw err;
-      dbData = JSON.parse(data);
-      dbData.push(userNotes);
-      let number = 1;
-      dbData.forEach((note, index) => {
-        note.id = number;
-        number++;
-        return dbData;
-      });
-      console.log(dbData);
-
-      stringData = JSON.stringify(dbData);
-
-      fs.writeFile('/db/db.json', stringData, (err, data) => {
-        if (err) throw err;
-      });
+      var dbData = JSON.parse(data);
+      res.send(dbData);
     });
-    res.send('Note has been written');
-  });
+  console.log("hit")
+})
 
-}
+app.post('/notes', (req, res) => {
+  //this should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+  const userNotes = req.body;
+
+  fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+    var dbData = JSON.parse(data);
+    dbData.push(userNotes);
+    let number = 1;
+    dbData.forEach((note, index) => {
+      note.id = number;
+      number++;
+      return dbData;
+    });
+    console.log(dbData);
+
+    stringData = JSON.stringify(dbData);
+
+    fs.writeFile('db/db.json', stringData, (err, data) => {
+      if (err) throw err;
+    });
+  });
+  res.send('Note has been written');
+});
+
+module.exports = app
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 // app.use is how we use the middleware GLOBALLY
 // could make a function and then put 
 //app.use(myFunction)
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //this is built in express to look at body of post request
 app.use(express.static('public'));
 
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
+app.use('/api', require('./routes/apiroutes'));
+app.use('/', require('./routes/htmlroutes'));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
